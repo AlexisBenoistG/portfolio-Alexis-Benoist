88f748a2487a6e0d7773d8611cb83f25382f9cb3
benoistalexis <alexisbenoistpro@gmail.com>
2025-11-24 19:48:19 +0100

    Feat: Change for the architecture with the BrowserRouter

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import MyName from "../sections/MyName";
+import About from "../sections/About";
+import Projects from "../sections/Projects";
+import Contact from "../sections/Contact";
+import MyName from "../sections/MyName";
+
+export default function Home() {
+  return (
+    <>
+      <MyName />
+      <About />
+      <Projects />
+      <Contact />
+    </>
+  );
+}
